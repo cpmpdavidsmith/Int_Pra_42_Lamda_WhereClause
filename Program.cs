189ecf04877df8ac5479c17d0eadc38ee99d08a6
David Smith <cpmpdavidsmith@icloud.com>
2024-01-16 13:49:07 -0500

    prac

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LamdaExample
+{
+    class QueryOperator
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+            double averageNumber = numbers.Where(num => num % 2 == 1).Average();
+            Console.WriteLine(averageNumber);
+            Console.ReadLine();
+        }
+    }
+}
